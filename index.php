fab8165677745c966411818e1175e5499c8f7226
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-01-22 22:25:22 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="philippe loctaux's website">
+    <meta name="keywords" content="philippe,loctaux,phil,philippeloctaux,филипп,локто,x4m3chki">
+    <meta name="google-site-verification" content="Ip8FH-JW2fMPsPgtilA0BigqNXq7wHGt_OCXhbcoJN0" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/f/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/f/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/f/favicon-16x16.png">
+    <link rel="manifest" href="/f/manifest.json">
+    <link rel="mask-icon" href="/f/safari-pinned-tab.svg" color="#ff0000">
+    <link rel="shortcut icon" href="/f/favicon.ico">
+    <meta name="msapplication-config" content="/f/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <title>Philippe Loctaux</title>
+    <style>
+    body{
+        font-family: verdana, arial, helvetica, sans-serif;
+        text-align: center;
+        margin: 1em 0px 1em;
+        font-size: 16px;
+    }a{
+        font-family: arial, sans-serif;
+        padding-left: 1px;
+        padding-right: 1px;
+    }a:hover{
+        font-weight: bolder;
+    }
+    </style>
+    <script>
+    var ss = [
+        { "rn" : "maybe sleeping" },
+        { "rn" : "tea powered person" },
+        { "rn" : "это фиаско братан!" },
+        { "rn" : "i need to do more stuff" },
+        { "rn" : "работаем ребята!" },
+        { "rn" : "non-stop chaos" },
+        { "rn" : "happy new year!" },
+        { "rn" : "check out <a href='http://db.philippeloctaux.com'><b>deadbead</b></a>!" },
+        { "rn" : "random carnage à la phil" },
+        { "rn" : "half french half russian and half drunk" },
+	{ "rn" : "vim lover" },
+	{ "rn" : "trashcan maker <a href='https://poubelle.online'>poubelle.online</a>" }
+    ];
+    var NuOb = Object.keys(ss).length;
+    function getRand() {
+        min = Math.ceil(0);
+        max = Math.floor(NuOb);
+        max = max--;
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+    var FiNu = getRand();
+    window.addEventListener("load", function() {
+        document.getElementById("q").innerHTML = ss[FiNu].rn;
+        console.log("status number " + FiNu + " displayed");
+    }, false);
+    </script>
+</head>
+<body>
+    <header>
+        <img src='data:image/gif;base64,R0lGODlhLAEhAIAAAAAAAP///ywAAAAALAEhAAAC/4SPqcvtD6OctNqLs968+w+G4kiW5omm6sq2bgDH8kzX9o3n+s73/g8MCofEIlBhTCqXzKbzCY3ekNKq9YrNaqXUrfcLDouf3bH5jE6Ly+q2+w0XsmuQqYF2wN9n+VxfVmcTSPcQ9aczp7egmAjTGHDouAc4GUmIgFgoOBlj6cm5CcDIYOdQ2mAIivPYScrnemnZKvpKS2krW5t7ixqq+FubaTvbGws7ugi1iyw8V8zsGywNqQq9Wi05nA18y13qPT37HY61vE1Orb2z+6nezm2eHs+u+o4+X4+dDt6NboXvLp86YeP6GRQ3DSDAgggPnnN4iV/Diff0pRpYMSDGa/8b7UH0qNBitme6jj2kGLFkMmMrkcUzsjDjEX2DUNLbaO2USV47PTIcuZNYUKCYqsT8KDIlS2cCf3IM2VIlU40EpSbQGZVnUS5JfZ7kcZPkPn9fn3ZNys/rWI4S/aAlyxVnWbVVldpsmvDsVqd171J1+9fHS7gX+859C9FvWrxrsRpex1gx4LZsfwxmclSyYJqRDweG+vixWrF0J1vWRGaoPL2PblJuXYm13pmB/U6lzBew2CW7iTa7Gs1uWaEdY8tdffyyZtvAkR7HHap3EumsiLoOjpJ4QdBxunv/7r06+PHky19Ubj69+vW/n7N/Dx+++Pj065Ofbz+//jYu+vsV/w9ggAIOSGCBBh6IYIIKLsjggQUAADs=' alt="philippeloctaux"><br>
+        <p><noscript>please enable javascript to see what i'm doing here</noscript><i id="q"></i></p>
+    </header>
+
+    <h2>hi i'm phil !</h2>
+    <p>aka <b>x4m3chki</b> aka <b>x4m3</b></p>
+    <p>this is how <a href="p.jpg">i look like</a>,<br>
+    and this is how <a href="p.mp3">i sound like</a>.</p>
+
+    <p>
+        i write my thoughts on <a href="https://twitter.com/philippeloctaux">twitter</a>,<br>
+        post pictures on <a href="https://www.instagram.com/philippeloctaux">instagram</a>,<br>
+        publish videos on <a href="https://www.youtube.com/philippeloctaux">youtube</a>,<br>
+        and put my code on <a href="https://github.com/x4m3">github</a>.<br>
+        i'm also on <a href="https://vk.com/philippeloctaux">vk</a>, <a href="https://reddit.com/user/philippeloctaux">reddit</a> and <a href="https://keybase.io/ploctaux">keybase</a>.
+    </p>
+
+    <p>
+        here's my <a href="p.asc">pgp key</a><br>
+        and my <a href="bitcoin:13mWKsipEsnAdnQ47ofSf7TEPbm7vsLBJd">bitcoin address</a>.
+    </p>
+
+    <p>
+        check out <a href="http://db.philippeloctaux.com">my blog</a><br>
+        and the <a href="https://timelapse.canvas.place">canvas.place timelapses</a>!</p>
+    <p>
+        you can <a href="tel:+33621156145">call</a> me or send me an <a href="mailto:loctauxphilippe@gmail.com?subject=hi%20phil!&amp;body=hi%20phil,%20i%20have%20something%20to%20tell%20you...">email</a>,<br>
+        or reach me via <a href="<?php $useragent = $_SERVER['HTTP_USER_AGENT']; if(preg_match('/Macintosh/',$useragent)) $os = 'imessage'; elseif(preg_match('/iPhone/',$useragent)) $os = 'sms'; else $os = 'sms'; echo $os; ?>:loctauxphilippe@gmail.com">imessage</a> or <a href="https://t.me/philippeloctaux">telegram</a>.
+    </p>
+
+    <footer>
+        <p>
+          <b>made with ❤️ on my laptop</b><br>
+          <i><?php echo date("dm.Hi", getlastmod()); ?></i>
+        </p>
+
+        &copy; Philippe Loctaux 2017<script>new Date().getFullYear()>2017&&document.write("-"+new Date().getFullYear());</script>
+    </footer>
+</body>
+</html>
