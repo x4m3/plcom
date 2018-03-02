c362cd8224d7190ffe832811f40eef2d8a350034
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-03-02 18:27:52 +0100

    added html minifier

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var htmlmin = require('gulp-htmlmin');
 
 gulp.task('src-serve', function() {
 	connect.server({
@@ -16,6 +17,12 @@ gulp.task('src-serve', function() {
 
 gulp.task('out-serve', function() {
 	/* do your shit */
+});
+
+gulp.task('html', function() {
+	return gulp.src('src/*.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('out'));
 });
 
 gulp.task('build', ['html', 'css', 'js']);
