e5801cb319165ea854527dbef9df2d022cfe8af2
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-03-02 18:26:38 +0100

    vim model lines removed
    it's in the vimrc now

--- src/p.js
+++ src/p.js
@@ -1,28 +1,26 @@
 var ss = [
-  { "rn" : "maybe sleeping" },
-  { "rn" : "tea powered person" },
-  { "rn" : "это фиаско братан!" },
-  { "rn" : "i need to do more stuff" },
-  { "rn" : "работаем ребята!" },
-  { "rn" : "non-stop chaos" },
-  { "rn" : "check out <a href='http://db.philippeloctaux.com'><b>deadbead</b></a>!" },
-  { "rn" : "random carnage à la phil" },
-  { "rn" : "half french half russian and half drunk" },
-  { "rn" : "vim lover" },
-  { "rn" : "trashcan maker <a href='https://poubelle.online'>poubelle.online</a>" },
-  { "rn" : "#заебись" }
+{ "rn" : "maybe sleeping" },
+{ "rn" : "tea powered person" },
+{ "rn" : "это фиаско братан!" },
+{ "rn" : "i need to do more stuff" },
+{ "rn" : "работаем ребята!" },
+{ "rn" : "non-stop chaos" },
+{ "rn" : "check out <a href='http://db.philippeloctaux.com'><b>deadbead</b></a>!" },
+{ "rn" : "random carnage à la phil" },
+{ "rn" : "half french half russian and half drunk" },
+{ "rn" : "vim lover" },
+{ "rn" : "trashcan maker <a href='https://poubelle.online'>poubelle.online</a>" },
+{ "rn" : "#заебись" }
 ];
 var NuOb = Object.keys(ss).length;
 function getRand() {
-  min = Math.ceil(0);
-  max = Math.floor(NuOb);
-  max = max--;
-  return Math.floor(Math.random() * (max - min)) + min;
+	min = Math.ceil(0);
+	max = Math.floor(NuOb);
+	max = max--;
+	return Math.floor(Math.random() * (max - min)) + min;
 }
 var FiNu = getRand();
 window.addEventListener("load", function() {
-  document.getElementById("q").innerHTML = ss[FiNu].rn;
-  console.log("status number " + FiNu + " displayed");
+	document.getElementById("q").innerHTML = ss[FiNu].rn;
+	console.log("status number " + FiNu + " displayed");
 }, false);
-
-/* vim: set ts=2 sw=2 sts=2 et : */
