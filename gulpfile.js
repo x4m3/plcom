7fee2191787bf4a82014dd77bb94fcf7cb1c6ba0
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-03-04 00:22:33 +0100

    added images compressions
    DOES NOT WORK

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var htmlmin = require('gulp-htmlmin'); /* shit-out the html */
 var uglify = require('gulp-uglify'); /* kick the js in the ass */
 var cleanCSS = require('gulp-clean-css'); /* shoot in the balls of the css */
 var svgmin = require('gulp-svgmin'); /* give a shower to the svg */
+var imagemin = require('gulp-imagemin'); /* the images */
 
 gulp.task('src-serve', function() {
 	connect.server({
@@ -56,6 +57,14 @@ gulp.task('svg', function() {
 
 gulp.task('copy', function() {
 	return gulp.src(['src/keybase.txt', 'src/names.mp3', 'src/pgp.asc'])
+		.pipe(gulp.dest('out'));
+});
+
+gulp.task('img', function() {
+	return gulp.src('src/*.{jpg,JPG,jpeg,JPEG,png,gif}')
+		.pipe(imagemin({
+			verbose: true
+		}))
 		.pipe(gulp.dest('out'));
 });
 
