8b5b76e6c0b03c0bcd186ece4759d5a1e6871f68
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-03-04 11:59:08 +0100

    removed image compressions bs
    i wasn't gaining a lot of space anyway

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ var htmlmin = require('gulp-htmlmin'); /* shit-out the html */
 var uglify = require('gulp-uglify'); /* kick the js in the ass */
 var cleanCSS = require('gulp-clean-css'); /* shoot in the balls of the css */
 var svgmin = require('gulp-svgmin'); /* give a shower to the svg */
-var imagemin = require('gulp-imagemin'); /* the images */
 
 gulp.task('src-serve', function() {
 	connect.server({
@@ -58,14 +57,6 @@ gulp.task('svg', function() {
 gulp.task('copy', function() {
 	return gulp.src(['src/keybase.txt', 'src/names.mp3', 'src/pgp.asc'])
 		.pipe(gulp.dest('out'));
-});
-
-gulp.task('img', function() {
-	return gulp.src('src/*.{jpg,JPG,jpeg,JPEG,png,gif}')
-		.pipe(imagemin({
-			verbose: true
-		}))
-		.pipe(gulp.dest('out/'));
 });
 
 gulp.task('default', function() {
