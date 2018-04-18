e8a36feca2960a843deacaf0471f4274373bf7d8
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-04-18 18:42:00 +0200

    renamed variable
    seems simpler

--- src/messages.js
+++ src/messages.js
@@ -1,4 +1,4 @@
-var ss = [
+var phil = [
 { "rn" : "maybe sleeping" },
 { "rn" : "tea powered person" },
 { "rn" : "это фиаско братан!" },
@@ -15,7 +15,7 @@ var ss = [
 { "rn" : "go top 1" },
 { "rn" : "playing <a href='http://canvas.place'>canvas.place</a> brb" }
 ];
-var NuOb = Object.keys(ss).length;
+var NuOb = Object.keys(phil).length;
 function getRand() {
 	min = Math.ceil(0);
 	max = Math.floor(NuOb);
@@ -24,6 +24,6 @@ function getRand() {
 }
 var FiNu = getRand();
 window.addEventListener("load", function() {
-	document.getElementById("q").innerHTML = ss[FiNu].rn;
+	document.getElementById("phil-status").innerHTML = phil[FiNu].rn;
 	console.log("status number " + FiNu + " displayed");
 }, false);
