d64b0bb2115c9a05b46da91a43a5ff9c78245583
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-04-19 21:36:16 +0200

    using arrays instead of objects
    i hope the vocabulary is right, still new to this

--- src/messages.js
+++ src/messages.js
@@ -1,46 +1,46 @@
 var phil = [
-{ "rn" : "maybe sleeping" },
-{ "rn" : "tea powered person" },
-{ "rn" : "это фиаско братан!" },
-{ "rn" : "i need to do more stuff" },
-{ "rn" : "работаем ребята!" },
-{ "rn" : "non-stop chaos" },
-{ "rn" : "check out <a href='http://db.philippeloctaux.com'><b>deadbead</b></a>!" },
-{ "rn" : "random carnage à la phil" },
-{ "rn" : "half french half russian and half drunk" },
-{ "rn" : "vim lover" },
-{ "rn" : "trashcan maker <a href='http://poubelle.online'>poubelle.online</a>" },
-{ "rn" : "#заебись" },
-{ "rn" : "sleep, eat, code, repeat" },
-{ "rn" : "go top 1" },
-{ "rn" : "playing <a href='http://canvas.place'>canvas.place</a> brb" }
+"maybe sleeping",
+"tea powered person",
+"это фиаско братан!",
+"i need to do more stuff",
+"работаем ребята!",
+"non-stop chaos",
+"check out <a href='http://db.philippeloctaux.com'><b>deadbead</b></a>!",
+"random carnage à la phil",
+"half french half russian and half drunk",
+"vim lover",
+"trashcan maker <a href='http://poubelle.online'>poubelle.online</a>",
+"#заебись",
+"sleep, eat, code, repeat",
+"go top 1",
+"playing <a href='http://canvas.place'>canvas.place</a> brb"
 ];
 
 var tool = [
-{ "used": "vim" },
-{ "used": "vscode" },
-{ "used": "linux" },
-{ "used": "windows" }
+"vim",
+"vscode",
+"linux",
+"windows"
 ];
 
-/* get the number of objects */
-var number_objects_phil = Object.keys(phil).length;
-var number_objects_tool = Object.keys(tool).length;
+/* get the number of elements */
+var number_elements_phil = Object.keys(phil).length;
+var number_elements_tool = Object.keys(tool).length;
 
-/* function to get a random number based on the number of objects */
-function get_random_number(number_objects) {
+/* function to get a random number based on the number of elements */
+function get_random_number(number_elements) {
 	min = Math.ceil(0);
-	max = Math.floor(number_objects);
+	max = Math.floor(number_elements);
 	max = max--;
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
 /* store the number of the object to display */
-var final_number_phil = get_random_number(number_objects_phil);
-var final_number_tool = get_random_number(number_objects_tool);
+var final_number_phil = get_random_number(number_elements_phil);
+var final_number_tool = get_random_number(number_elements_tool);
 
 /* display the object in the html on page load */
 window.addEventListener("load", function() {
-	document.getElementById("phil-rn").innerHTML = phil[final_number_phil].rn;
-	document.getElementById("tool-used").innerHTML = tool[final_number_tool].used;
+	document.getElementById("phil-rn").innerHTML = phil[final_number_phil];
+	document.getElementById("tool-used").innerHTML = tool[final_number_tool];
 }, false);
