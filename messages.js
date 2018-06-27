bb7382a4fc5c411542bdf20045c2a53809fec88c
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-06-27 02:52:48 +0200

    removed semicolons and quotes

--- messages.js
+++ messages.js
@@ -1,21 +1,21 @@
 var messages = [
-];
+]
 
 /* get the number of elements */
-var numberElements = Object.keys(messages).length;
+var numberElements = Object.keys(messages).length
 
 /* function to get a random number based on the number of elements */
-function getRandomNumber(numberElements) {
-  min = Math.ceil(0);
-  max = Math.floor(numberElements);
-  max = max--;
-  return Math.floor(Math.random() * (max - min)) + min;
+function getRandomNumber (numberElements) {
+  min = Math.ceil(0)
+  max = Math.floor(numberElements)
+  max = max--
+  return Math.floor(Math.random() * (max - min)) + min
 }
 
 /* store the number of the object to display */
-var finalNumber = getRandomNumber(numberElements);
+var finalNumber = getRandomNumber(numberElements)
 
 /* display the object in the html on page load */
-window.addEventListener("load", function() {
-  document.getElementById("message").innerHTML = messages[finalNumber];
-}, false);
+window.addEventListener('load', function () {
+  document.getElementById('message').innerHTML = messages[finalNumber]
+}, false)
