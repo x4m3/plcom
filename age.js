95e234a7d32f8973b7ba448e7c69d8699e7be7a0
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-06-29 16:53:02 +0200

    reduced the age calculator to a single line

--- age.js
+++ age.js
@@ -1,10 +1,3 @@
-function calculateAge (dob) {
-  var diffDates = Date.now() - dob.getTime()
-  var age = new Date(diffDates)
-
-  return Math.abs(age.getUTCFullYear() - 1970)
-}
-
 window.addEventListener('load', function () {
-  document.getElementById('age').innerHTML = calculateAge(new Date(2000, 11, 4))
+  document.getElementById('age').innerHTML = Math.abs(new Date(Date.now() - (new Date(2000, 11, 4).getTime())).getUTCFullYear() - 1970)
 }, false)
