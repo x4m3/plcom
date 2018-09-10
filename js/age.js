14e2980356b5b6c98ba355ed2530caaf9bb7c33a
Philippe Loctaux <p@philippeloctaux.com>
2018-09-10 17:54:41 +0200

    redid the alignment

--- js/age.js
+++ js/age.js
@@ -1,3 +1,3 @@
 window.addEventListener('load', function () {
-  document.getElementById('age').innerHTML = Math.abs(new Date(Date.now() - (new Date(2000, 11, 4).getTime())).getUTCFullYear() - 1970)
+	document.getElementById('age').innerHTML = Math.abs(new Date(Date.now() - (new Date(2000, 11, 4).getTime())).getUTCFullYear() - 1970)
 }, false)
--- js/messages.js
+++ js/messages.js
@@ -1,13 +1,13 @@
 var messages = [
-  '🇫🇷 🇷🇺',
-  'i love vim',
-  '#заебись',
-  'sleep, eat, code, repeat',
-  'maybe playing <a href="http://canvas.place">canvas.place</a>',
-  'laughing to <a href="https://www.reddit.com/r/ProgrammerHumor">r/programmerhumor</a> memes brb',
-  'ебусь голову ежедневно',
-  '<a href="https://www.google.com/search?q=wubba+lubba+dub+dub">wubba lubba dub dub!</a>',
-  'echo cEBwaGlsaXBwZWxvY3RhdXguY29tCg== | base64 --decode'
+	'🇫🇷 🇷🇺',
+	'i love vim',
+	'#заебись',
+	'sleep, eat, code, repeat',
+	'maybe playing <a href="http://canvas.place">canvas.place</a>',
+	'laughing to <a href="https://www.reddit.com/r/ProgrammerHumor">r/programmerhumor</a> memes brb',
+	'ебусь голову ежедневно',
+	'<a href="https://www.google.com/search?q=wubba+lubba+dub+dub">wubba lubba dub dub!</a>',
+	'echo cEBwaGlsaXBwZWxvY3RhdXguY29tCg== | base64 --decode'
 ]
 
 /* get the number of elements */
@@ -15,9 +15,9 @@ var numberElements = Object.keys(messages).length
 
 /* function to get a random number based on the number of elements */
 function getRandomNumber (numberElements) {
-  var min = Math.ceil(0)
-  var max = Math.floor(numberElements)
-  return Math.floor(Math.random() * (max - min)) + min
+	var min = Math.ceil(0)
+	var max = Math.floor(numberElements)
+	return Math.floor(Math.random() * (max - min)) + min
 }
 
 /* store the number of the object to display */
@@ -25,5 +25,5 @@ var finalNumber = getRandomNumber(numberElements)
 
 /* display the object in the html on page load */
 window.addEventListener('load', function () {
-  document.getElementById('message').innerHTML = messages[finalNumber]
+	document.getElementById('message').innerHTML = messages[finalNumber]
 }, false)
