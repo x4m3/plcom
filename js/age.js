4cb1fbdacded9fb53dfed6b6f1daf9d3a2d4ecae
Philippe Loctaux <p@philippeloctaux.com>
2018-11-05 18:50:04 +0100

    age.js: removed and hard coding age

--- js/age.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener('load', function () {
-	document.getElementById('age').innerHTML = Math.abs(new Date(Date.now() - (new Date(2000, 11, 4).getTime())).getUTCFullYear() - 1970)
-}, false)
